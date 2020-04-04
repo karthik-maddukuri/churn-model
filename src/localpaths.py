6ebd36a44209ee709a66f9615fb46de60c7de51d
Karthik Maddukuri <kmaddukuri6@gmail.com>
2020-04-04 17:58:08 -0500

    Created file paths for training and testing status"

--- src/localpaths.py
+++ src/localpaths.py
@@ -11,3 +11,10 @@ PROCESSED_DATA_DIRECTORY = os.path.join(DATA_DIRECTORY,'processed')
 # File paths
 RAW_DATA_PATH = os.path.join(RAW_DATA_DIRECTORY, 'WA_Fn-UseC_-Telco-Customer-Churn.csv')
 
+
+# File paths for raw trian test split
+
+X_TRAIN_RAW_PATH = os.path.join(RAW_DATA_DIRECTORY, 'X_train.csv')
+X_TEST_RAW_PATH = os.path.join(RAW_DATA_DIRECTORY, 'X_test.csv')
+Y_TRAIN_RAW_PATH = os.path.join(RAW_DATA_DIRECTORY, 'y_train.csv')
+Y_TEST_RAW_PATH = os.path.join(RAW_DATA_DIRECTORY, 'y_test.csv')
