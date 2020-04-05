43926b4f51985f4f28b3a8d20b4ca476c9687169
Karthik Maddukuri <kmaddukuri6@gmail.com>
2020-04-04 20:47:42 -0500

    Created function to one hot encode the categorical features

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -7,6 +7,7 @@ sys.path.append('.')
 import click
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 #Local imports
 from src.localpaths import *
@@ -24,10 +25,11 @@ def featurize_X_train(X_train):
     """
     X_train = drop_customer_id(X_train)
     X_train = transform_binary_categorical(X_train)
+    X_train = one_hot_encode_categorical_features(X_train)
 
     return X_train
 
-    
+
 
 @cli.command()
 def create_featurized_data():
@@ -68,6 +70,29 @@ def transform_binary_categorical(X_train):
     X_train['PaperlessBilling'] = X_train['PaperlessBilling'].map({'Yes': 1, 'No': 0})
 
     return X_train
+
+
+
+def one_hot_encode_categorical_features(X_train):
+    """
+    This one hot encodes the categorical features, add these to X-train,
+    then drops the original columsn. Returns the transformed X_train data as dataframe
+    """
+    cols_to_one_hot_encode = X_train.dtypes[X_train.dtypes == 'object'].index
+
+    ohe = OneHotEncoder(drop='first',sparse=False)
+    ohe.fit(X_train[cols_to_one_hot_encode])
+
+    ohe_features = ohe.transform(X_train[cols_to_one_hot_encode])
+    ohe_feature_names = ohe.get_feature_names(cols_to_one_hot_encode)
+
+    ohe_df = pd.DataFrame(ohe_features, columns = ohe_feature_names)
+
+    X_train = X_train.assign(**ohe_df)
+    X_train = X_train.drop(columns=cols_to_one_hot_encode )
+
+    return X_train
+
 
 
 def transform_target(y_train):
