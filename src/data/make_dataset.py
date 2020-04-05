720e1b9af37d9198735b163fa8f48ba2e9d7d1be
Karthik Maddukuri <kmaddukuri6@gmail.com>
2020-04-04 20:08:45 -0500

    Added final paramter load_training_data fucntion

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -64,12 +64,15 @@ def create_clean_training_data():
 
 
 # Helper function for loading training and testing data, instead of using the localpaths functions everytime
-def load_training_data(clean = False):
+def load_training_data(clean = False, final=False):
     """ Return the X_train and y_train data if they exist
     """
     if clean:
         X_train = pd.read_csv(X_TRAIN_CLEAN_PATH)
         y_train = pd.read_csv(Y_TRAIN_CLEAN_PATH)
+    elif final:
+        X_train = pd.read_csv(X_TRAIN_FEATURIZED_PATH)
+        y_train = pd.read_csv(Y_TRAIN_FEATURIZED_PATH)
     else:
         X_train = pd.read_csv(X_TRAIN_RAW_PATH)
         y_train = pd.read_csv(Y_TRAIN_RAW_PATH)
