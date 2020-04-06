324ef3e011d6c6d278b0db2a996556543b5bdebc
Karthik Maddukuri <kmaddukuri6@gmail.com>
2020-04-05 21:32:48 -0500

    Added fucntion to print all the metrics

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -65,5 +65,16 @@ def store_model_and_results(model, X_train, y_train):
     df_results.to_csv(model_results_filepath, index=False)
 
 
+
+def print_model_results(model, X_train, y_train):
+    """
+    This prints the model evaluation metrics
+    """
+    metrics = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc']
+    for metric in metrics:
+        metric_value = np.mean(cross_val_score(model, X_train, y_train['Churn'], cv=5, scoring=metric))
+        print(f'{metric}: {metric_value:.2f}')
+
+
 if __name__ == "__main__":
     pass
